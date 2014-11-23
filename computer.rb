8736f46267e81eb7c865dc835602b18caa37720a
chapmanderek <chapmanderek@gmail.com>
2014-11-23 08:39:45 +0000

    Filter by max price added and tested

--- computer.rb
+++ computer.rb
@@ -34,7 +34,8 @@ class Printer
     
     def print_menu
         puts "1.  View all information for all computers in database"
-        puts "2.  View all computers in database"          
+        puts "2.  View all computers in database" 
+        puts "3.  Filter computers by max price"
         puts "E =  Exit"
         print "-->"
     end 
@@ -45,6 +46,21 @@ class Printer
     
     def print_basic_info(for_item)
         puts "#{for_item.get_brand} #{for_item.get_model}"
+    end 
+end 
+
+class Filter
+    def initialize 
+    end 
+    
+    def by_max_price(max, allcomputers)
+        puts "The following computers are less than $#{max}"
+        allcomputers.each_value do |computer|
+            if computer.get_price <= max
+                puts "The #{computer.get_brand} #{computer.get_model} costs $#{computer.get_price}"
+            end 
+        end 
+                
     end 
 end 
 
@@ -63,11 +79,13 @@ end
 allcomputers = Hash.new()
 print_info = Printer.new 
 allcomputers = seed(allcomputers)
+filters = Filter.new  
 
 breakout = false
 while breakout != true 
     print_info.print_menu()
-    input = gets.chomp
+    input = gets.chomp 
+    
     input.downcase!
     
     case input
@@ -75,11 +93,21 @@ while breakout != true
             allcomputers.each_value {|computer| print_info.print_all_attributes(computer)}
         when '2' 
             allcomputers.each_value {|computer| puts "#{computer.get_brand} #{computer.get_model}"}
+        when '3'
+            print "Enter the maximum price: "
+            input = gets.chomp
+            max = input.to_i
+            if (max > 0)
+                filters.by_max_price(max, allcomputers)
+            else
+                puts "Sorry I didn't understand that." 
+            end 
         when 'e'
             breakout = true 
         else 
             puts "Invalid input.  Try again." 
     end
+    
     puts ""
 end
 
